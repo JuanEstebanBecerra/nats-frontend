c1d1dfa6b93cb725d7c33ef290ec929eca5844b7
Juan Esteban Becerra <estebanbecerradev@gmail.com>
2024-11-14 08:09:23 -0500

    Update index.vue

--- nats-frontend/pages/index.vue
+++ nats-frontend/pages/index.vue
@@ -15,16 +15,11 @@ async function connectToNats() {
 		const nc = await connect({
 			servers: 'ws://localhost:8080',
 			token: 's3cr3t',
-		}) // Usa la URL del servidor de NATS
+		})
 
         const sc = StringCodec();
+		const sub = nc.subscribe('hello')
 
-		console.log('Conectado a NATS')
-
-		// Suscribirse a un canal (subject)
-		const sub = nc.subscribe('hello') // Cambia 'mi.canal' por el canal que deseas escuchar
-
-		// Manejar los mensajes que lleguen
 		;(async () => {
 			for await (const msg of sub) {
 				console.log(`Mensaje recibido: ${sc.decode(msg.data)}`)
